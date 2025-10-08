75270b31f9d619433edb7f4e1011af0061ab9116
Victor Nascimento <victormatheusnasc@gmail.com>
2025-10-07 23:54:38 -0300

    Update streamlit_app.py
    Adjust the API call.

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,10 @@ if ingredients_list is not None:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen )
+        sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
+      
     #st.write("You selected:", ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders (ingredients , name_on_order )
@@ -42,8 +46,6 @@ if ingredients_list is not None:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
+
 
 
